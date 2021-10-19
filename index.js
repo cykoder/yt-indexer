bbc765bc4dbe3ee5e3b452644463c8d69af28c2d
Samuel Hellawell <sshellawell@gmail.com>
2021-10-19 12:23:47 +0100

    duckduckgo searching

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import path from 'path';
 import fs from 'fs';
 import xmlParser from 'fast-xml-parser';
 import Fastify from 'fastify';
+import qs from 'qs';
+import cheerio from 'cheerio';
 import { URL } from 'url';
 import searchYoutube from './innertube.js';
 
@@ -61,33 +63,42 @@ function crawlURI(crawler, uri, priority = 5) {
   }
 }
 
+function buildVideoUri(videoId) {
+  return `https://www.youtube.com/watch?v=${videoId}`;
+}
+
+function cleanYTUri(uri) {
+  return uri.replace('https://www.youtube.com/oembed?url=', '').replace('&format=json', '');
+}
+
 // Takes a video ID (or generates a random one) and creates an oembed URI that we can use
 // to gather public metadata of the video. Then it will insert the URI into the crawler que
-async function crawlYTVideo(crawler, videosCollection, id) {
+async function crawlYTVideo(crawler, videosCollection, id, highPriority = 1) {
   // If queue is already processing quite a few requests then dont generate
   // random videos URIs. Set a timeout to check again later
   if (!id && (skipAddingNew || crawler.queueSize > 64)) {
-    console.log('Queue size too large, skipping random video ID generation');
+    // console.log('Queue size too large, skipping random video ID generation');
     setTimeout(() => {
       crawlYTVideo(crawler, videosCollection);
     }, 5000);
     return;
   }
 
-  // We use oembed here so that random video check requests dont effect our API rate limits if used
-  const videoUri = `https://www.youtube.com/watch?v=${id || randomVideoId()}`;
+  // We use oembed here to check if a YouTube video is valid, and if so get some basic info
+  const videoUri = buildVideoUri(id || randomVideoId());
   const url = `https://www.youtube.com/oembed?url=${videoUri}&format=json`;
-  if (id) { // If ID is provided double check that its not in the database
-    const docCount = await videosCollection.count({ uri: videoUri });
-    if (docCount === 0) {
-      crawlURI(crawler, url, 1);
-    }
+  if (id) { // Insert known ID with a high priority (1) to the crawler
+    const videoUri = cleanYTUri(url);
+    insertVideo(videosCollection, { uri: videoUri });
+    crawlURI(crawler, url, highPriority);
   } else {
     // Insert with random priority so that we can still process
     // some random URIs even if random search is producing alot of results
-    crawlURI(crawler, url, crypto.randomInt(1, 3));
+    crawlURI(crawler, url, crypto.randomInt(highPriority, 3));
   }
 
+  // ID wasnt provided, assuming wanting to continue the random
+  // generation infinite loop
   if (!id) {
     setTimeout(() => {
       crawlYTVideo(crawler, videosCollection);
@@ -97,9 +108,9 @@ async function crawlYTVideo(crawler, videosCollection, id) {
 
 // Gets a random word from the dictionary and searches it with the
 // innertube API. It will add the video uris to the crawler que at high priority
-async function crawlRandomSearch(crawler, videosCollection) {
+async function crawlRandomYTSearch(crawler, videosCollection) {
   // Set skip adding new if que is too large until its nearly all been processed
-  if (!skipAddingNew && crawler.queueSize > 128) {
+  if (!skipAddingNew && crawler.queueSize > 256) {
     skipAddingNew = true;
   } else if (skipAddingNew && crawler.queueSize <= 4) {
     skipAddingNew = false;
@@ -108,7 +119,7 @@ async function crawlRandomSearch(crawler, videosCollection) {
   // If que is growing too fast, dont perform more random searches
   if (!skipAddingNew) {
     const randomQueryString = wordsList[crypto.randomInt(0, wordsListCount)];
-    console.log('Searching for:', randomQueryString);
+    console.log('Searching YouTube for:', randomQueryString);
 
     // Get a list of video IDs from this mess of an API result
     try {
@@ -128,10 +139,74 @@ async function crawlRandomSearch(crawler, videosCollection) {
   }
 
   setTimeout(() => {
-    crawlRandomSearch(crawler, videosCollection);
+    crawlRandomYTSearch(crawler, videosCollection);
   }, randomSearchTimeout);
 }
 
+async function crawlRandomDuckDuckGoSearch(crawler, videosCollection, nextRequest) {
+  // Fire off a POST request to DuckDuckGo's HTML site with prebuilt params or a random query
+  const { data } = await axios({
+    method: 'POST',
+    url: 'https://html.duckduckgo.com/html/',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    data: qs.stringify(nextRequest || {
+      q: 'site:youtube.com/watch?v=' + randomChar(),
+    }),
+  });
+
+  // Parse HTML contents with cheerio to extract next request data
+  const $ = cheerio.load(data);
+  const nextFormInputFields = $('form[action=\'/html/\'] :input[type=hidden]');
+  const nextRequestData = {};
+  nextFormInputFields.map((index) => {
+    const field = nextFormInputFields[index].attribs;
+    nextRequestData[field.name] = field.value;
+  });
+
+  // Regex to extract all YouTube urls
+  const ytUrlRegex = /(https?:\/\/([^=]*)youtu([^=]*)[^ ]*)/g;
+
+  // Regex to extract YouTube video IDs
+  const ytVideoIDRegex = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+
+  // Extract all youtube video IDs from HTML
+  // then adds the known video IDs to the crawler
+  const videoIds = data.match(ytUrlRegex).map(url => {
+    const urlIdMatches = url.match(ytVideoIDRegex);
+    if (urlIdMatches && urlIdMatches.length >= 2) {
+      const videoId = urlIdMatches[1].substr(0, 11);
+      return videoId;
+    }
+  })
+  .filter((url, index, self) => self.indexOf(url) === index);
+
+  // Consider these URLS as highest priority (0)
+  videoIds.forEach(videoId => crawlYTVideo(crawler, videosCollection, videoId, 0));
+  console.log('Added', videoIds.length, 'duck videos');
+
+  // Wait a bit before searching next page
+  setTimeout(() => {
+    crawlRandomDuckDuckGoSearch(crawler, videosCollection, nextRequestData);
+  }, randomSearchTimeout * 2);
+}
+
+async function insertVideo(videosCollection, { uri, title = '', authorName = '', authorUrl = '', description = '' }) {
+  try {
+    await videosCollection.updateOne({ uri }, {
+      $set: {
+        uri,
+        title,
+        authorName,
+        authorUrl,
+        description,
+      },
+    }, { upsert: true });
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 // Callback for when a page has been crawled
 // typically would be omebed JSON or RSS feed
@@ -139,7 +214,7 @@ async function onCrawled(error, res, done, opts) {
   try {
     const { uri } = res.options;
     const { crawler, videosCollection } = opts;
-    const videoUri = uri.replace('https://www.youtube.com/oembed?url=', '');
+    const videoUri = cleanYTUri(uri);
     if (error || res.statusCode === 500) {
       failedCounter++;
       console.error(error || `Server error: ${res.statusCode} ${res.body} ${uri}`);
@@ -151,39 +226,20 @@ async function onCrawled(error, res, done, opts) {
       // Unauthorized means that the video exists but is flagged as not embeddable
       // only way to get info would be through the youtube API - which we can do later
       // so for now lets just store it in the database as a valid uri
-      try {
-        console.log('\nCrawled unauthed URI:', uri)
-        await videosCollection.updateOne({ uri: videoUri }, {
-          $set: {
-            uri: videoUri,
-            title: '',
-            authorName: '',
-            authorUrl: '',
-            thumbnail: '',
-            description: '',
-          },
-        }, { upsert: true });
-      } catch (e) {
-        console.error(e);
-      }
+      console.log('\nCrawled unauthed URI:', uri);
+      insertVideo(videosCollection, { uri: videoUri });
     } else if (res.statusCode === 200) {
-      const { title, author_name, author_url, thumbnail_url } = JSON.parse(res.body);
-      // console.log('\nIndexed URI:', videoUri);
+      const { title, author_name, author_url } = JSON.parse(res.body);
+      console.log('\nIndexed URI:', videoUri);
 
-      try {
-        await videosCollection.updateOne({ uri: videoUri }, {
-          $set: {
-            uri: videoUri,
-            title,
-            authorName: author_name,
-            authorUrl: author_url,
-            thumbnail: thumbnail_url,
-            description: '',
-          },
-        }, { upsert: true });
-      } catch (e) {
-        console.error(e);
-      }
+      // Insert video in the background
+      insertVideo(videosCollection, {
+        uri: videoUri,
+        title,
+        authorName: author_name,
+        authorUrl: author_url,
+        description: '',
+      });
 
       // Get RSS feed of channel and crawl their videos
       const ytChannelStr = 'https://www.youtube.com/channel/';
@@ -281,7 +337,8 @@ async function main() {
   // Do some crawling
   console.log('Starting crawling...');
   if (!process.env.DISABLE_SEARCH) {
-    crawlRandomSearch(crawler, videosCollection);
+    crawlRandomDuckDuckGoSearch(crawler, videosCollection);
+    crawlRandomYTSearch(crawler, videosCollection);
   }
   crawlYTVideo(crawler, videosCollection);
 }
